74418d8c36223fef7cf127e5678aa994e263d725
skyrbe <harsha.venkatram@sakhatech.com>
2017-05-17 18:33:44 +0530

    Off to a fresh start

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <h1>
-        {  `React + Redux = 🤷` }
-      </h1>
-    );
-  }
-}
-ReactDOM.render(<App />, document.querySelector('.container'));
+//Fresh start
