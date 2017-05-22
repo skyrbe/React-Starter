9cd228e74d9eaa76c4fcffa16ee40c271cdd9fab
skyrbe <harsha.venkatram@sakhatech.com>
2017-05-22 10:50:07 +0530

    Day 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-//Fresh start
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+//Write some HTML that can be rendered on the screen
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+
+//Plug this HTML into the DOM
+ReactDOM.render(<App/>,document.getElementById("react_container"));
